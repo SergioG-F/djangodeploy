b50405113b359da469acbf5ffde49ecfac826259
Sergio Guzman <guzmanfernandezsergio@gmail.com>
2024-10-24 11:45:54 -0500

    depklt

--- demodjango/settings.py
+++ demodjango/settings.py
@@ -15,9 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY',default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangodeploy-0izr.onrender.com', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -113,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 if not DEBUG:
     STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
